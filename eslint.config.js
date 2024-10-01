6a07b62b5e2924b00dec681ed77e6d6b9e80a6f9
Mateusz Kupiec <157227025+m-kupiec@users.noreply.github.com>
2024-10-01 15:06:26 +0200

    chore: use starter-app-react-ts-tailwind as the project starter

---
+++ eslint.config.js
@@ -0,0 +1,43 @@
+import js from "@eslint/js";
+import globals from "globals";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  { ignores: ["dist", "coverage", "**/*.js"] },
+  {
+    extends: [
+      js.configs.recommended,
+      jsxA11y.flatConfigs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+    ],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    settings: { react: { version: "detect" } },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      react,
+    },
+    rules: {
+      ...react.configs.recommended.rules,
+      ...react.configs["jsx-runtime"].rules,
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+    },
+  },
+);
