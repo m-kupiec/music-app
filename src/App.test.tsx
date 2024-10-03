3087303d11f88ee90f4c804c60eaf527e1bab0c0
Mateusz Kupiec <157227025+m-kupiec@users.noreply.github.com>
2024-10-03 13:06:46 +0200

    feat: display welcome screen when no Spotify tokens are available on opening the app

---
+++ src/App.test.tsx
@@ -0,0 +1,52 @@
+import { describe, it, vi, expect, afterEach } from "vitest";
+import { cleanup, render, screen } from "@testing-library/react";
+import App from "./App.tsx";
+import * as screensUtils from "./feat/screens/utils.ts";
+
+vi.mock("./feat/screens/WelcomeScreen.tsx", () => {
+  return {
+    default: function WelcomeScreen() {
+      return <main data-testid="welcome-screen"></main>;
+    },
+  };
+});
+
+/*
+The return value of useAccountConnectionStatus() is irrelevant here (and so made null),
+  as it is only used as an argument in getScreenName(),
+  the return value of which is mocked anyway.
+*/
+vi.mock("./feat/accountConnection/hooks.ts", () => {
+  return {
+    useAccountConnectionStatus() {
+      return null;
+    },
+  };
+});
+
+describe("The app", () => {
+  const getScreenNameSpy = vi.spyOn(screensUtils, "getScreenName");
+
+  afterEach(() => {
+    getScreenNameSpy.mockReset();
+    cleanup();
+  });
+
+  it("should be able to start with the welcome screen", () => {
+    getScreenNameSpy.mockReturnValue("welcome");
+
+    render(<App />);
+
+    const element = screen.queryByTestId("welcome-screen");
+    expect(element).not.toBeNull();
+  });
+
+  it("should be able not to start with the welcome screen", () => {
+    getScreenNameSpy.mockReturnValue("none");
+
+    render(<App />);
+
+    const element = screen.queryByTestId("welcome-screen");
+    expect(element).toBeNull();
+  });
+});
