9b0c31a41be51e99cfe750b310f045c4dda460dd
Mateusz Kupiec <157227025+m-kupiec@users.noreply.github.com>
2024-10-05 13:46:55 +0200

    chore: configure additional Spotify brand colors for Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,10 @@ export default {
     extend: {
       colors: {
         "spotify-green": "#1ED760",
+        "spotify-green-highlight": "#1FDF64",
         "spotify-white": "#FFFFFF",
         "spotify-black": "#121212",
+        "spotify-black-text": "#000000",
       },
     },
   },
