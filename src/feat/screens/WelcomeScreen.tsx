ffdccde81ee0d086255041c2a152a08cd86bc781
Mateusz Kupiec <157227025+m-kupiec@users.noreply.github.com>
2024-10-05 14:02:57 +0200

    chore: align welcome screen styles and texts with Spotify branding

--- src/feat/screens/WelcomeScreen.tsx
+++ src/feat/screens/WelcomeScreen.tsx
@@ -1,19 +1,21 @@
 function WelcomeScreen() {
   return (
     <main
-      className="flex h-screen flex-wrap content-center justify-center"
+      className="flex h-screen flex-wrap content-center justify-center bg-spotify-black"
       data-testid="welcome-screen"
     >
-      <header className="h-20 text-center">
-        <h1 className="mb-4 text-4xl">Music App</h1>
+      <header className="h-20 text-center text-spotify-white">
+        <h1 className="mb-7 text-5xl font-semibold tracking-tighter">
+          Music App
+        </h1>
         <p data-testid="spotify-account-connection-prompt">
-          Please connect your Spotify account to proceed
+          Please connect your Spotify account to proceed.
         </p>
         <button
-          className="text-spotify-black bg-spotify-green hover:bg-spotify-black hover:text-spotify-green mt-8 rounded-full px-8 py-2 transition-colors"
+          className="text-spotify-black-text hover:bg-spotify-green-highlight mt-5 h-12 rounded-full bg-spotify-green px-8 font-semibold transition-colors"
           data-testid="spotify-account-connection-button"
         >
-          Connect Spotify Account
+          Connect
         </button>
       </header>
     </main>
